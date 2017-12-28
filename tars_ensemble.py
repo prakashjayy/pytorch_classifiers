fd691eeaec64010d80e033e6278b9303cd3a3a1a
sachin <sachinchandra.999@gmail.com>
2017-12-28 16:22:44 +0530

    replaced print with loggers

--- tars_ensemble.py
+++ tars_ensemble.py
@@ -6,8 +6,9 @@ We will be using the following ensembling strategies:
 """
 
 import pandas as pd
+import logging
 
-
+logging.basicConfig(filename='ensemble.log', filemode='w', level=logging.DEBUG)
 models_used = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "densenet121", "densenet169", "densenet201",
 "squeezenet1_0", "squeezenet1_1", "inception_v3", "vgg11", "vgg13", "vgg16", "vgg19", "vgg11_bn", "vgg13_bn", "vgg16_bn", "vgg19_bn", "alexnet"]
 models_loc = ["submission/"+i+"_False_freeze.csv" for i in models_used]
@@ -21,8 +22,8 @@ for i in models_loc:
     models_combined["file"] = f["file"].values
     models_combined[i.rsplit("/")[-1].rsplit(".")[0]] = f["label"].values
 
-print(models_combined.shape)
-print(models_combined.head())
+logging.debug(models_combined.shape)
+logging.debug(models_combined.head())
 models_combined.to_csv("submissions/ensemble1_intermediate.csv", index=False)
 cols = list(models_combined.columns)
 cols.pop(0)
